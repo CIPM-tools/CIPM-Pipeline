47fcbb0a96c1199547efc1c3e775ef0b16584823
David Monschein <david.monschein@novatec-gmbh.de>
2019-07-29 14:02:00 +0200

    Started with WebUI.

--- dmodel.root/dmodel.pipeline.rt.rest/src/main/java/dmodel/pipeline/rt/rest/core/CoreRestController.java
+++ dmodel.root/dmodel.pipeline.rt.rest/src/main/java/dmodel/pipeline/rt/rest/core/CoreRestController.java
@@ -3,10 +3,10 @@ package dmodel.pipeline.rt.rest.core;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController("/core")
 public class CoreRestController {
 
-	@GetMapping("ping")
+	@GetMapping("/ping")
 	public String ping() {
 		return "true";
 	}
