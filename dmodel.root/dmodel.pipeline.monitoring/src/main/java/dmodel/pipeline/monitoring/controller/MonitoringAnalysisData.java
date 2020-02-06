1caf283cd3cc4a3bf6b582f233bb186e2f22e5be
David Monschein <david.monschein@novatec-gmbh.de>
2020-02-06 13:45:15 +0100

    Updated project structure and extended overhead logging.

--- dmodel.root/dmodel.pipeline.monitoring/src/main/java/dmodel/pipeline/monitoring/controller/MonitoringAnalysisData.java
+++ dmodel.root/dmodel.pipeline.monitoring/src/main/java/dmodel/pipeline/monitoring/controller/MonitoringAnalysisData.java
@@ -16,6 +16,16 @@ public class MonitoringAnalysisData {
 
 	private long internalOverhead = 0;
 
+	private long serviceCalls = 0;
+	private long branches = 0;
+	private long loops = 0;
+	private long internals = 0;
+
+	private float avgServiceCall = 0;
+	private float avgBranches = 0;
+	private float avgLoops = 0;
+	private float avgInternals = 0;
+
 	public MonitoringAnalysisData() {
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			public void run() {
@@ -29,19 +39,47 @@ public class MonitoringAnalysisData {
 	}
 
 	public synchronized void exitServiceCallOverhead(String serviceId, long start) {
-		this.serviceCallOverhead = (System.nanoTime()) - start;
+		long duration = (System.nanoTime()) - start;
+		this.serviceCallOverhead += duration;
+		this.serviceCalls++;
+
+		this.avgServiceCall += duration;
+		if (this.avgServiceCall > 0) {
+			this.avgServiceCall /= 2;
+		}
 	}
 
 	public synchronized void exitBranchOverhead(String branchId, long start) {
-		this.branchOverhead = (System.nanoTime()) - start;
+		long duration = (System.nanoTime()) - start;
+		this.branchOverhead += duration;
+		this.branches++;
+
+		this.avgBranches += duration;
+		if (this.avgBranches > 0) {
+			this.avgBranches /= 2;
+		}
 	}
 
 	public synchronized void exitLoopOverhead(String loopId, long start) {
-		this.loopOverhead = (System.nanoTime()) - start;
+		long duration = (System.nanoTime()) - start;
+		this.loopOverhead += duration;
+		this.loops++;
+
+		this.avgLoops += duration;
+		if (this.avgLoops > 0) {
+			this.avgLoops /= 2;
+		}
 	}
 
 	public synchronized void internalOverhead(String internalActionId, long start) {
-		this.internalOverhead = (System.nanoTime()) - start;
+		long duration = (System.nanoTime()) - start;
+		this.internalOverhead += duration;
+		this.internals++;
+
+		this.avgInternals += duration;
+		if (this.avgInternals > 0) {
+			this.avgInternals /= 2;
+		}
 	}
 
 	public void writeOverhead() {
@@ -65,6 +103,22 @@ public class MonitoringAnalysisData {
 			writer.append(String.valueOf(loopOverhead));
 			writer.append(";");
 			writer.append(String.valueOf(internalOverhead));
+			writer.append(";");
+			writer.append(String.valueOf(serviceCalls));
+			writer.append(";");
+			writer.append(String.valueOf(branches));
+			writer.append(";");
+			writer.append(String.valueOf(loops));
+			writer.append(";");
+			writer.append(String.valueOf(internals));
+			writer.append(";");
+			writer.append(String.valueOf(avgServiceCall));
+			writer.append(";");
+			writer.append(String.valueOf(avgBranches));
+			writer.append(";");
+			writer.append(String.valueOf(avgLoops));
+			writer.append(";");
+			writer.append(String.valueOf(avgServiceCall));
 			writer.append(System.lineSeparator());
 			writer.flush();
 			writer.close();
